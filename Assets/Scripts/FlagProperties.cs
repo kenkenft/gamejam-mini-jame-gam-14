5c0f747b23e86e74150797516bef0fa01d0ebea4
Ken Tang <kenftang@gmail.com>
2023-02-04 11:01:31 +0000

    Add SetUp, SetIsPickedUp, and RaiseLowerFlag
    FlagProperties has additional methods.
    
    SetUp sets the flagID, isPickedUp array,
    the flag's initial point value, and finds
    the gameobject that hodls the flag sprite.
    
    SetIsPickedUp checks whether the flag has
    been picked up. There are 2 indexes for parts
    1 and 2 of a level's game state.
    
    RaiseLowerFlag moves the flag sprite up or down
    to indicate that a flag has been interacted with.

--- Assets/Scripts/FlagProperties.cs
+++ Assets/Scripts/FlagProperties.cs
@@ -6,13 +6,39 @@ public class FlagProperties : MonoBehaviour
 {
     bool[] isPickedUp = {false, false};
     int flagID, pointValue;
+    GameObject flagMain;
+    Vector3 mask = new Vector3(0.27f,0f,0f);
 
-
-
-    // Start is called before the first frame update
-    void Start()
+    public void SetUp(int assignedID, int initialValue)
     {
-        
+        isPickedUp[0] = false;
+        isPickedUp[1] = false;
+        flagID = assignedID;
+        pointValue = initialValue;
+        Transform[] allTransforms = GetComponentsInChildren<Transform>();
+        foreach(Transform transform in allTransforms)
+        {
+            if(transform.name == "FlagMain")
+            {
+                flagMain = transform.gameObject;
+                break;
+            }
+        }
+    }
+
+    public void RaiseLowerFlag(bool isRaising)
+    {
+        if(isRaising)
+            mask[1] = -0.07f;
+        else
+            mask[1] = -0.57f;
+            
+        flagMain.transform.position = mask;
+    }
+
+    public void SetIsPickedUp(int levelState, bool state)
+    {
+        isPickedUp[levelState] = state;
     }
 
 }
